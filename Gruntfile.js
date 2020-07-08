164083cc5eedaea532681e0e519a0f7327606454
Robert Lucian <r.lucian13@yahoo.com>
2020-07-08 22:13:10 +0300

    nav working on desktop

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        concat : {
+            files : {
+                src : [
+                    'ui-src/js/test.js'
+                ],
+                dest : 'ui/js/main.js',
+                options : {
+                    interrupt: true,
+                    separator : '\n\n',
+                }
+            }
+        },
+        sass : {
+            style : {
+                src : [
+                    'ui-src/sass/style.scss'
+                ],
+                dest : 'ui/css/style.css'
+            },
+
+            vendor : {
+                src : [
+                    'ui-src/sass/vendor.scss'
+                ],
+                dest : 'ui/css/vendor.css'
+            }
+        },
+        
+        watch : {
+            js : {
+                files : [
+                    'ui-src/js/*.js',
+                    'ui-src/js/**/*.js',
+                    'ui-src/js/**/**/*.js',
+                ],
+                tasks : ['concat']
+            },
+
+            sass : {
+                files : [
+                    'ui-src/sass/*.scss',
+                    'ui-src/sass/**/*.scss',
+                    'ui-src/sass/**/**/*.scss',
+                ],
+
+                tasks : ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+}
